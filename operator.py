39e68bfc4a4a98d2933c7ec1b11643655fe35ede
Kayizzi-Abdul <kayizziabdul18@gmail.com>
2022-08-19 18:41:19 +0300

    trying to add

---
+++ operator.py
@@ -0,0 +1,53 @@
+# x = 6
+# y = 3
+
+# print(f"x +  y = {x + y}")
+
+# print(f"x - y = {x - y }")
+
+# print(f"x * y = {x * y}")
+
+# print(f"x / y = {x / y}")
+
+# print(f"x % y = {x % y}")
+
+# print(f"x // y = {x // y}")
+
+# print(f"x ** y = {x ** y}")
+
+# a = 5
+
+# a +=5
+
+# print(a)
+
+# b = 20
+
+# b -= 5
+
+# print(b)
+
+# c = 8
+
+# c &= 3
+
+# print(c)
+
+# d = 7
+
+# d |= 5
+
+# print(d)
+
+# voting_age = 18
+# is_registered = False
+
+# my_age = int(input("enter your age"))
+# is_reg = int(input("enter 1 if registered and 0 if not"))
+
+# if my_age >= voting_age and is_reg == True:
+#     print("you are allowed to vote")
+
+# else:
+#     print("go back home")
+
